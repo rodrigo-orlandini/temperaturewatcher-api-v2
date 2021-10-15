06786bdf1a90118dafeab3b2f805dac9778fb242
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2021-10-14 22:49:55 -0300

    Checking the traceback to fix temperature register creation.

--- models/controller.py
+++ models/controller.py
@@ -9,7 +9,7 @@ class ControllerModel(db.Model):
 
     @classmethod
     def find_last(cls) -> "ControllerModel":
-        return cls.query.order_by(db.desc(cls.id)).limit(1).first()
+        return cls.query.order_by(db.desc(cls.id)).limit(1).first() 
 
     def save_to_database(self):
         db.session.add(self)
